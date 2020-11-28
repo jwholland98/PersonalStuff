d4b1bc7937d22aa9d0999b0ccf29786e0d24a755
Jesse Holland <holland.jesse55@gmail.com>
2020-11-28 11:25:18 -0600

    ECC added

--- Networks/final/client.py
+++ Networks/final/client.py
@@ -1,11 +1,49 @@
 import socket
 import sys
+from tinyec import registry
+import tinyec.ec as ec
+from Crypto.Cipher import AES
+import hashlib, secrets, binascii
+import pickle
+
+curve = registry.get_curve('brainpoolP256r1')
+
 
 def encode(message):
     return message.encode()
 
 def decode(message):
     return message.decode("utf-8")
+
+def encryptAES(msg, secretkey):
+    aesCipher = AES.new(secretkey, AES.MODE_GCM)
+    ciphertext, authTag = aesCipher.encrypt_and_digest(msg)
+    return (ciphertext, aesCipher.nonce, authTag)
+
+def decrypt_AES_GCM(ciphertext, nonce, authTag, secretKey):
+    aesCipher = AES.new(secretKey, AES.MODE_GCM, nonce)
+    plaintext = aesCipher.decrypt_and_verify(ciphertext, authTag)
+    return plaintext
+
+def eccTo256BitKey(point):
+    sha = hashlib.sha256(int.to_bytes(point.x, 32, 'big'))
+    sha.update(int.to_bytes(point.y, 32, 'big'))
+    return sha.digest()
+
+def encryptECC(message, pubkey):
+    ciphertextPrivKey = secrets.randbelow(curve.field.n)
+    sharedECCKey = ciphertextPrivKey * pubkey
+    secretKey = eccTo256BitKey(sharedECCKey)
+    ciphertext, nonce, authTag = encryptAES(message, secretKey)
+    ciphertextPubKey = ciphertextPrivKey * curve.g
+    return (ciphertext, nonce, authTag, ciphertextPubKey)
+
+def decryptECC(encryptedMsg, privkey):
+    (ciphertext, nonce, authTag, ciphertextPubKey) = encryptedMsg
+    sharedECCKey = privkey * ciphertextPubKey
+    secretKey = eccTo256BitKey(sharedECCKey)
+    plaintext = decrypt_AES_GCM(ciphertext, nonce, authTag, secretKey)
+    return plaintext
 
 def main():
     msgFromClient = "HELLO"
@@ -17,13 +55,43 @@ def main():
 
     msgFromServer = UDPClientSocket.recvfrom(bufferSize)
     print(decode(msgFromServer[0]))
+    choice = ""
+    while choice != "1":
+        print("""Options:\n
+            1.) Reverse Shell
+            2.) [TODO - Add other options]""")
+        choice = input()
+    inp = ""
+    while not inp == "JHDONE":
+        inp = input()
+        inp = "JH" + inp
+        cmd=inp.encode()
+        #privkey = secrets.randbelow(curve.field.n)
+        privkey = 55523049895079075860098996757830212235119640612963685438975685046735125749529
+        pubkey = privkey * curve.g
+        encryptedMsg = encryptECC(cmd, pubkey)
+        encryptedMsgObj = {
+            'ciphertext': encryptedMsg[0],
+            'nonce': encryptedMsg[1],
+            'authTag': encryptedMsg[2],
+            'ciphertextPubKeyX': encryptedMsg[3].x, 
+            'ciphertextPubKeyY': encryptedMsg[3].y
+        }
+        if choice == "1":
+            encryptedMsgObj['type'] = "001"
 
-    cmd = ""
-    while not cmd == "DONE":
-        cmd = input()
-        bytesToSend = encode(cmd)
+        bytesToSend = pickle.dumps(encryptedMsgObj)
         UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-        print(decode(msgFromServer[0]))
+
+        bytesAddressPair = UDPClientSocket.recvfrom(bufferSize)
+        responseMsgObj = bytesAddressPair[0]
+        responseMsgObj = pickle.loads(responseMsgObj)
+        responseMsg = (responseMsgObj['ciphertext'], responseMsgObj['nonce'], 
+            responseMsgObj['authTag'], ec.Point(curve, responseMsgObj['ciphertextPubKeyX'], responseMsgObj['ciphertextPubKeyY']))
+        try:
+            response = decode(decryptECC(responseMsg, privkey))
+        except:
+            response = "Error decoding command :: invalid key"
+        print(response)
 
 main()
--- Networks/final/server.py
+++ Networks/final/server.py
@@ -1,22 +1,61 @@
 import socket
 import sys
 import subprocess
+from tinyec import registry
+import tinyec.ec as ec
+from Crypto.Cipher import AES
+import hashlib, secrets, binascii
+import pickle
 
 localIP = "127.0.0.1"
 localPort = 8123
 bufferSize = 1024
+curve = registry.get_curve('brainpoolP256r1')
 
+
+#credit to https://cryptobook.nakov.com/asymmetric-key-ciphers/ecc-encryption-decryption for encryption and decryption implementation
 def encode(message):
     return message.encode()
 
 def decode(message):
     return message.decode("utf-8")
 
+def encryptAES(msg, secretkey):
+    aesCipher = AES.new(secretkey, AES.MODE_GCM)
+    ciphertext, authTag = aesCipher.encrypt_and_digest(msg)
+    return (ciphertext, aesCipher.nonce, authTag)
+
+def decrypt_AES_GCM(ciphertext, nonce, authTag, secretKey):
+    aesCipher = AES.new(secretKey, AES.MODE_GCM, nonce)
+    plaintext = aesCipher.decrypt_and_verify(ciphertext, authTag)
+    return plaintext
+
+def eccTo256BitKey(point):
+    sha = hashlib.sha256(int.to_bytes(point.x, 32, 'big'))
+    sha.update(int.to_bytes(point.y, 32, 'big'))
+    return sha.digest()
+
+def encryptECC(message, pubkey):
+    ciphertextPrivKey = secrets.randbelow(curve.field.n)
+    sharedECCKey = ciphertextPrivKey * pubkey
+    secretKey = eccTo256BitKey(sharedECCKey)
+    ciphertext, nonce, authTag = encryptAES(message, secretKey)
+    ciphertextPubKey = ciphertextPrivKey * curve.g
+    return (ciphertext, nonce, authTag, ciphertextPubKey)
+
+def decryptECC(encryptedMsg, privkey):
+    (ciphertext, nonce, authTag, ciphertextPubKey) = encryptedMsg
+    sharedECCKey = privkey * ciphertextPubKey
+    secretKey = eccTo256BitKey(sharedECCKey)
+    plaintext = decrypt_AES_GCM(ciphertext, nonce, authTag, secretKey)
+    return plaintext
+
 def main():
     UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
     UDPServerSocket.bind((localIP, localPort))
-    message = ""
+    #privkey = secrets.randbelow(curve.field.n)
+    privkey = 55523049895079075860098996757830212235119640612963685438975685046735125749529
+    pubkey = privkey * curve.g
 
     print("UDP server up and listening")
     while(True):
@@ -28,16 +67,38 @@ def main():
             UDPServerSocket.sendto(bytesToSend, address)
         while not message == "DONE":
             bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-            message = decode(bytesAddressPair[0])
+            encryptedMsgObj = bytesAddressPair[0]
             address = bytesAddressPair[1]
-            print("Message from Client:{}".format(message))
-            print("Client IP Address:{}".format(address))
-            if message !="DONE":
+            encryptedMsgObj = pickle.loads(encryptedMsgObj)
+            encryptedMsg = (encryptedMsgObj['ciphertext'], encryptedMsgObj['nonce'], 
+                encryptedMsgObj['authTag'], ec.Point(curve, encryptedMsgObj['ciphertextPubKeyX'], encryptedMsgObj['ciphertextPubKeyY']))
+            try:
+                message = decode(decryptECC(encryptedMsg, privkey))
+            except:
+                message = "err"
+            print("Message from Client: {}".format(message))
+            print("Client IP Address: {}".format(address))
+            output = ""
+            if message[0] != 'J' or message[1] != 'H':
+                output = "Invalid use of protocol"
+            if message == "err":
+                output = "Error decoding command :: invalid key or invalid use of protocol"
+            message = message[2:]
+            if message == "DONE":
+                output = "Goodbye!"
+            elif encryptedMsgObj['type'] == "001" and output == "":
                 output = subprocess.getoutput(message)
-                bytesToSend = encode(output)
-                UDPServerSocket.sendto(bytesToSend, address)
-        bytesToSend = encode("Goodbye!")
-        UDPServerSocket.sendto(bytesToSend, address)
+            output = output.encode()
+            responseMsg = encryptECC(output, pubkey)
+            responseMsgObj = {
+                'ciphertext': responseMsg[0],
+                'nonce': responseMsg[1],
+                'authTag': responseMsg[2],
+                'ciphertextPubKeyX': responseMsg[3].x, 
+                'ciphertextPubKeyY': responseMsg[3].y
+            }
+            bytesToSend = pickle.dumps(responseMsgObj)
+            UDPServerSocket.sendto(bytesToSend, address)
 
 
 main()
